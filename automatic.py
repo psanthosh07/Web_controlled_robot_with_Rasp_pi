5099abe7ca83645442fa3576c34d07c44a571455
psanthosh07 <58479203+psanthosh07@users.noreply.github.com>
2023-10-26 02:11:14 +0530

    Update automatic.py

--- automatic.py
+++ automatic.py
@@ -10,49 +10,51 @@ right_motor = Motor(forward=24, backward=25)
 front_sensor = DistanceSensor(echo=19, trigger=26)
 back_sensor = DistanceSensor(echo=21, trigger=20)
 
+def forward():
+    left_motor.forward()
+    right_motor.forward()
+
+def stop():
+    left_motor.stop()
+    right_motor.stop()
+
 def avoid_obstacle():
-    # Forward movement
-    left_motor.forward()
-    right_motor.forward()
-    time.sleep(1)  # Move forward for 1 second
+    forward()
+    while True:
+        # Check front sensor
+        if front_sensor.distance < 0.3:
+            # Obstacle in front, turn right
+            left_motor.forward()
+            right_motor.backward()
+            time.sleep(1)  # Turn right for 1 second
+        else:
+            # No obstacle in front, continue forward
+            forward()
+        
+        time.sleep(1)  # Continue forward for 1 second
 
-    # Stop
-    left_motor.stop()
-    right_motor.stop()
-    time.sleep(0.5)  # Stop for 0.5 seconds
+        # Stop
+        stop()
+        time.sleep(0.5)  # Stop for 0.5 seconds
 
-    # Check front sensor
-    if front_sensor.distance < 0.3:
-        # Obstacle in front, turn right
-        left_motor.forward()
-        right_motor.backward()
-        time.sleep(1)  # Turn right for 1 second
-    else:
-        # No obstacle in front, continue forward
-        left_motor.forward()
-        right_motor.forward()
+        # Check back sensor
+        if back_sensor.distance < 0.3:
+            # Obstacle behind, turn left
+            left_motor.backward()
+            right_motor.forward()
+            time.sleep(1)  # Turn left for 1 second
+        else:
+            # No obstacle behind, continue forward
+            forward()
+        
+        time.sleep(1)  # Continue forward for 1 second
 
-    time.sleep(1)  # Continue forward for 1 second
-
-    # Stop
-    left_motor.stop()
-    right_motor.stop()
-    time.sleep(0.5)  # Stop for 0.5 seconds
-
-    # Check back sensor
-    if back_sensor.distance < 0.3:
-        # Obstacle behind, turn left
-        left_motor.backward()
-        right_motor.forward()
-        time.sleep(1)  # Turn left for 1 second
-
-    # Continue forward
-    left_motor.forward()
-    right_motor.forward()
+        # Stop
+        stop()
+        time.sleep(0.5)  # Stop for 0.5 seconds
 
 try:
     while True:
         avoid_obstacle()
 except KeyboardInterrupt:
-    left_motor.stop()
-    right_motor.stop()
+    stop()
